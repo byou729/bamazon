a870fbcfa895029736f8dfdb6cb4b685f9ce5fc5
Ben You <byou729@gmail.com>
2018-07-23 22:57:12 -0500

    results in screenshoots

--- bamazonCustomers.js
+++ bamazonCustomers.js
@@ -12,60 +12,47 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    productDisplay();
+    connection.query("SELECT * FROM products", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        productToBuy();
+    });
 });
-
-function productDisplay() {
-    inquirer
-        .prompt({
-            name: "department",
-            type: "input",
-            message: "Following are the details of products that are available for you to buy."
-        })
-        .then(function (answer) {
-            var query = "SELECT id_item, product_name, price FROM product WHERE ?";
-            connection.query(query, {department: department.artist}, function (err, res) {
-                for (var i = 0; i < res.length; i++) {
-                    console.log("ID: " + res[i].id_item + " || name: " + res[i].product_name + " || price: " + res[i].price);
-                }
-                productToBuy();
-            });
-        });
-}
 
 function productToBuy() {
     inquirer
         .prompt({
             name: "action",
-            type: "list",
-            message: "What is the ID of the product they would like to buy?",
-            choices: [
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-            ]
-        })
-        .then(function (answer) {
-            switch (answer.action) {
-                case "Quantity of the products":
-                    quantityToBuy();
-                    break;
-            }
-        });
-}
-
-function quantityToBuy() {
-    inquirer
-        .prompt({
-            name: "action",
             type: "input",
-            message: "What is the quantity of the product they would like to buy?"
+            message: "What is the ID of the product they would like to buy?",
         })
         .then(function (answer) {
-            if (process.argv[0] < product_id.product_quantity) {
-                console.log("Insufficient quantity!")
-            } else {
-                product_id.product_quantity =- product_id.quantity - input;
-                console.log("total cost is" + process.argv[0]*product.id_product_price)
-            }
-        });
-}
+            console.log(answer.item_id);
+            connection.query("SELECT * FROM products WHERE ?", { item_id: answer.item_id }, function (err, res) {
 
+                quantityToBuy();
+
+            });
+        });
+
+    }
+
+        function quantityToBuy() {
+            inquirer
+              .prompt({
+                name: "quantity",
+                type: "input",
+                message: "What is the quantity of the product they would like to buy?"
+              })
+              .then(function(answer) {
+                connection.query("SELECT * FROM products WHERE ?", { song: answer.stock_quantity }, function(err, res) {
+                  if (process.argv[0] > answer.stock_quantity) {
+                      console.log("Insufficient quantity!")
+                  } else {
+                      answer.stock_quantity =- answer.stockquantity - process.argv[0];
+                      console.log(process.argv[0]);
+                  }
+                  productToBuy();
+              });
+          });
+        }
